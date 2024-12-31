1b365a6a1605ed1b35d8d58a38eac46d2f74a301
Zaw Min Htut <zawminhtutx@gmail.com>
2024-12-31 09:48:04 +0630

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+export const dynamic = "force-dynamic";
+
+export default function RootLayout({
+    children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <main>{children}</main>;
+}
